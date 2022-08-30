5df131fe7d3b9e704abfad0f095177231dfe3699
Rafael Rodrigues <rafaelkunzler@gmail.com>
2022-08-30 10:40:24 -0300

    Fazendo os primeiros testes de movimentacao

---
+++ xadrez-console/Entities/xadrez/PartidaDeXadrez.cs
@@ -0,0 +1,45 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class PartidaDeXadrez
+    {
+        public Tabuleiro tab { get; private set; }
+        private int turno;
+        private Cor jogadorAtual;
+        public bool terminada { get; private set; }
+
+        public PartidaDeXadrez()
+        {
+            tab = new Tabuleiro(8, 8);
+            turno = 1;
+            jogadorAtual = Cor.Branca;
+            terminada = false;
+            colocarPecas();
+        }
+
+        public void executaMovimento(Posicao origem, Posicao destino)
+        {
+            Peca p = tab.retirarPeca(origem);
+            p.incrementarQteMovimento();
+            Peca pecaCapturada = tab.retirarPeca(destino);
+            tab.colocarPeca(p, destino);
+          
+        }
+
+        private void colocarPecas()
+        {
+            tab.colocarPeca(new Torre(Cor.Branca, tab), new PosicaoXadrez('a',1).toPosicao());
+            tab.colocarPeca(new Torre(Cor.Branca, tab), new PosicaoXadrez('h',1).toPosicao());
+            tab.colocarPeca(new Rei(Cor.Branca, tab), new PosicaoXadrez('d', 1).toPosicao());
+
+            tab.colocarPeca(new Torre(Cor.Preta, tab), new PosicaoXadrez('a', 8).toPosicao());
+            tab.colocarPeca(new Torre(Cor.Preta, tab), new PosicaoXadrez('h', 8).toPosicao());
+            tab.colocarPeca(new Rei(Cor.Preta, tab), new PosicaoXadrez('d', 8).toPosicao());
+
+
+
+        }
+
+    }
+}
--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -1,11 +1,12 @@
 ﻿using tabuleiro;
+using xadrez;
 
 namespace xadrez_console
 {
     internal class Tela 
     {
 
-        public static void imprimirTabuleiro(Tabuleiro tab)
+        public static void ImprimirTabuleiro(Tabuleiro tab)
         {
 
             for (int i = 0; i < tab.linha; i++)
@@ -30,6 +31,13 @@ namespace xadrez_console
             Console.WriteLine("  a b c d e f g h");
         }
 
+        public static PosicaoXadrez lerPosicaoXadrez()
+        {
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse(s[1] + "");
+            return new PosicaoXadrez(coluna, linha);
+        }
         public static void imprimirPeca(Peca peca)
         {
             if(peca.cor == Cor.Branca)
