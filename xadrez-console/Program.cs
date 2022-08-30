1d148845913366cc93312417382bd97b61d4eff5
Rafael Rodrigues <rafaelkunzler@gmail.com>
2022-08-30 09:42:59 -0300

    Posicao xadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -7,21 +7,11 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Console.WriteLine(pos);
 
-                tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 9));
-                tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 2));
+            Console.WriteLine(pos.toPosicao());
 
-                Tela.imprimirTabuleiro(tab);
-
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
         }
     }
 
